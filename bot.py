52af14e6029efbe81cb310ad66d24cb4c279e543
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-02 20:29:28 +0400

    tried to add number

--- bot.py
+++ bot.py
@@ -18,6 +18,8 @@ ADMIN_ID = 540235460790976512
 pic = open("img/anon_dp.png", 'rb')
 pfp = pic.read()
 
+pic_count = 0
+
 @client.event
 async def on_ready():
     await client.user.edit(avatar=pfp)
@@ -27,7 +29,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    embed=discord.Embed(title="Anonymous", description=message.content, color=0xFFFF00)
+    embed=discord.Embed(title="Anonymous", description=str(pic_count)+'\n'+message.content, color=0xFFFF00)
+    pic_count += 1
     channel = client.get_channel(FEEDBACK_CHANNEL)
     user = client.get_user(ADMIN_ID)
     if "Direct Message" in str(message.channel):
