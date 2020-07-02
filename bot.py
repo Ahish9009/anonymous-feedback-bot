e8edb5c06e3f51fb1b9c2e8bf9e6e34f57f5f4ad
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-02 16:50:01 +0400

    first commit

---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    embed=discord.Embed(title="Anonymous", description=message.content, color=0xFFFF00)
+    channel = client.get_channel(728019657671311370)
+    if "Direct Message" in str(message.channel):
+        # print(message.author)
+        # print(message.channel)
+        # await channel.send(message.content)
+        await channel.send(embed=embed.set_image(url=message.attachments[0].url))
+
+
+client.run(TOKEN)
